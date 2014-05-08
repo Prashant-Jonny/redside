1a61f06f49805717f5e8593d00dead2f9c8c440e
nemerle <nemerle5@gmail.com>
2014-05-09 00:10:11 +0200

    Switched unit test 'assert_equal' parameters to make error reports more accurate

--- test/lib/test_bitstream.rb
+++ test/lib/test_bitstream.rb
@@ -6,10 +6,10 @@ require 'stringio'
 class TestBitStream < Test::Unit::TestCase
   def test_initial_state
     bs = BitStream.new("")
-    assert_equal(bs.get_readable_bits(), 0)
-    assert_equal(bs.is_byte_aligned, false)
+    assert_equal(0,bs.get_readable_bits())
+    assert_equal(false,bs.is_byte_aligned)
     bs = BitStream.new("11")
-    assert_equal(bs.get_readable_bits(), 16)
+    assert_equal(16,bs.get_readable_bits())
   end
 
   def test_byte_alignment
@@ -17,25 +17,25 @@ class TestBitStream < Test::Unit::TestCase
     bs.store_bits(3, 3)
     bs.byte_align(true, true)             # after aligning to next bytes we should have 8 bits readable
     assert_equal(bs.get_readable_bits, 8)
-    assert_equal(bs.get_bits(3), 3)
-    assert_equal(bs.get_bits(5), 0)       # the rest of auto-added bits should be zeroes
+    assert_equal(3,bs.get_bits(3))
+    assert_equal(0,bs.get_bits(5))       # the rest of auto-added bits should be zeroes
   end
 
   def test_plain_bits
     bs = BitStream.new("")
     bs.store_bits(2, 1)
-    assert_equal(bs.get_bits(2), 1)
+    assert_equal(1,bs.get_bits(2))
   end
 
   def test_packed_bits
     bs = BitStream.new("")    
     bs.store_packed_bits(3, 11)
-    assert_equal(bs.get_packed_bits(3), 11)
+    assert_equal(11,bs.get_packed_bits(3))
   end
   def test_string
     bs = BitStream.new("")    
     bs.store_string("Strings of Destiny!")
-    assert_equal(bs.get_string(), "Strings of Destiny!")
+    assert_equal("Strings of Destiny!",bs.get_string())
   end
   def test_mixed_stores
     bs = BitStream.new("")
@@ -44,9 +44,9 @@ class TestBitStream < Test::Unit::TestCase
     bs.store_string("Test me baby one more time")
     payload = [87].pack("c*")
     bs.store_bit_array(payload, payload.size*8)
-    assert_equal(bs.get_packed_bits(3), 5)
-    assert_equal(bs.get_bits(3), 4)
-    assert_equal(bs.get_string(), "Test me baby one more time")
-    assert_equal(bs.get_bit_array(payload.size*8),payload)
+    assert_equal(5,bs.get_packed_bits(3))
+    assert_equal(4,bs.get_bits(3))
+    assert_equal("Test me baby one more time",bs.get_string())
+    assert_equal(payload,bs.get_bit_array(payload.size*8))
   end
 end
